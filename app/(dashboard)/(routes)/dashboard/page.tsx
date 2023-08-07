c5caf9e7f6a3539e72db6155be1c874087eea961
Lekan <jimoridwanlekan@gmail.com>
2023-08-07 15:10:25 +0100

    Install clerk authentication

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const DashBoardPage: FC<pageProps> = ({}) => {
+  return <div>page</div>
+}
+
+export default DashBoardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const LandingPage: FC<pageProps> = ({}) => {
+  return <div>page</div>
+}
+
+export default LandingPage;
