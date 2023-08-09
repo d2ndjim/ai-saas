06247ba5adc08cb80d108f4bafb7d1c2e7ec827d
Lekan <jimoridwanlekan@gmail.com>
2023-08-09 13:50:14 +0100

    Complete landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,15 @@
-import { FC } from 'react'
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
 
-interface pageProps {
-  
-}
-
-const LandingPage: FC<pageProps> = ({}) => {
-  return <div>page</div>
-}
+const LandingPage = () => {
+  return (
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+  );
+};
 
 export default LandingPage;
